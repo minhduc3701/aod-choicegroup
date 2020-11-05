8dbe075d44b00db0b3508face0992640d39b7bda
nmd <minh.duc.3701@gmail.com>
2020-11-05 10:06:35 +0700

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Toggle } from "aod-dependencies/Toggle";
+import { initializeIcons } from "aod-dependencies/@uifabric/icons";
+import { Wrapper } from "./AppStyle";
+import CustomChoiceGroup from "aod-dependencies/ChoiceGroup/CustomChoiceGroup";
+import { IChoiceGroupOption } from "aod-dependencies/ChoiceGroup/ChoiceGroup.types";
+import "./App.css";
+
+initializeIcons();
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState<string>("");
+  const [disabled, setDisabled] = React.useState<boolean>(false);
+  const onChangeMode = () => {
+    if (darkMode === "dark") {
+      setDarkMode("light");
+    }
+    if (darkMode !== "dark") {
+      setDarkMode("dark");
+    }
+  };
+  const onChangeDisable = () => {
+    setDisabled(!disabled);
+  };
+
+  const optionsChoiceGroup: IChoiceGroupOption[] = [
+    { key: "A", text: "Option A" },
+    { key: "B", text: "Option B", disabled: true },
+    { key: "C", text: "Option C" },
+    { key: "D", text: "Option D" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper theme={darkMode}>
+        <div className="toggle-wrapper">
+          <Toggle label="Dark mode" onChange={onChangeMode} />
+          <Toggle label="Disable" onChange={onChangeDisable} />
+        </div>
+        <CustomChoiceGroup
+          options={optionsChoiceGroup}
+          defaultSelectedKey="B"
+          label="Pick one"
+          darkMode={darkMode}
+          disabled={disabled}
+        />
+      </Wrapper>
     </div>
   );
 }
